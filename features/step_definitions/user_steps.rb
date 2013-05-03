a1054c055a062909e32f1ea85a5e8ad8722e15b6
Chris Kim <cj3kim@gmail.com>
2013-05-03 15:19:06 -0700

    Completed cucumber step: User logins, registers a gift card, and sees the report

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -8,26 +8,37 @@ Given /^I login into the app$/ do
   fill_in("user_email", with: email)
   fill_in("user_password", with: password)
   click_button("Sign in")
-
-  save_and_open_page
 end
 
 And /^I go to my dashboard$/ do
-
+  click_link("My Dashboard")
 end
 
 And /^I register a new gift card$/ do
-
+  click_link("gift card")
 end
 
 And /^I fill in the gift card fields$/ do
+  vendor = "Motoko's Tea Shop"
+  description = "Very good tea. You should try some, buddy!"
+  card_value = "45.0"
+  email = "kaoru@su.org"
+  first_name = "kaoru"
+  last_name = "su"
 
+  fill_in("gift_card_vendor", with: vendor)
+  fill_in("gift_card_description", with: description)
+  fill_in("gift_card_card_value", with: card_value)
+  fill_in("gift_card_recipient_email", with: email)
+  fill_in("gift_card_recipient_first_name", with: first_name)
+  fill_in("gift_card_recipient_last_name", with: last_name)
 end
 
-When /^I click submit$/ do
-
+When /^I click submit I create a gift card$/ do
+  click_button("Create Gift card")
+  save_and_open_page
 end
 
-Then /^I am redirected to my dashboard$/ do
-
+Then /^the page should contain this text: (.*)$/ do |text|
+  page.should have_content(text)
 end
--- features/user.feature
+++ features/user.feature
@@ -4,8 +4,12 @@ Feature: User
      And I go to my dashboard
      And I register a new gift card
      And I fill in the gift card fields
-     When I click submit
-     Then I am redirected to my dashboard
+     When I click submit I create a gift card 
+     Then the page should contain this text: Motoko's Tea Shop
+     Then the page should contain this text: $45.00
+     Then the page should contain this text: kaoru@su.org
+     Then the page should contain this text: kaoru
+     Then the page should contain this text: su
 
 
 
