c1918ebefad3abd2e4f28de73ecc56fb4fe2fd12
Chris Kim <cj3kim@gmail.com>
2013-05-03 13:45:07 -0700

    Users can see registered cards appropriately

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -10,6 +10,10 @@ class GiftCard < ActiveRecord::Base
     self.card_value = dollars_to_pennies(self.card_value)
   end
 
+  def card_value_in_dollars 
+    pennies_to_dollars(self.card_value)
+  end
+
   def pennies_to_dollars(pennies)
     float_pennies = pennies.to_f #pennies is an integer value
     divisor = 100
