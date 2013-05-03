bb7560991fadc5ede66dfd1c41272e7905c6673a
Chris Kim <cj3kim@gmail.com>
2013-05-02 21:59:14 -0700

    Users can have many gift cards, but a gift card belongs only to one user at most

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -1,5 +1,6 @@
 class GiftCard < ActiveRecord::Base
+  belongs_to :user
+
   attr_accessible :vendor, :card_value, :card_code,
     :recipient_email, :recipient_first_name, :recipient_last_name, :user
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :gift_cards
+
   attr_accessible :email, :dob, :first_name, :last_name
 
 end
