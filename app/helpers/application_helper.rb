b0691f67e85b9fd48a79049795113ca135a9a0f9
Chris Kim <cj3kim@gmail.com>
2013-05-03 11:40:22 -0700

    Completed basic user authentication setup with devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def generate_sign_in_or_out_link
+    if user_signed_in?
+      link_to "sign out", "/users/sign_out", method: :delete
+    else
+      link_to "sign in", "/users/sign_in", method: :get
+    end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TamaGift::Application.routes.draw do
-  devise_for :users
-
   root to: 'users#home'
 
+  devise_for :users
+  resources :users
 end
