1fe36b9601eef12650ac2e8b9d1a1ff9111e9b28
Chris Kim <cj3kim@gmail.com>
2013-05-02 22:06:05 -0700

    Configured giftcard and user factories

--- spec/factories/gift_cards.rb
+++ spec/factories/gift_cards.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :gift_card do
+  factory :gift_card do |f|
+    f.vendor nil
+    f.card_value nil
+    f.card_code nil
+    f.recipient_email nil
+    f.recipient_first_name nil
+    f.recipient_last_name nil
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user do |f|
+    f.email nil
+    f.first_name nil
+    f.last_name nil
+    f.dob nil
   end
 end
