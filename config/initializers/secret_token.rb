3bdda3905057e1ad95b9c096abd34665202860c0
Chris Kim <cj3kim@gmail.com>
2013-05-02 20:54:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TamaGift::Application.config.secret_token = 'aebafbcb268743be7103b735c515ab1e4ef01e0e71a290ec400db7b8b7375d80f76fb00ad7c457987674a4b6434ea9e1ceeb1a7e70a7ea914f6fafd0316eb996'
