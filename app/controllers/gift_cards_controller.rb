4f1dd257e5ddc21fdfae4901d44cc314ee38c4ff
Chris Kim <cj3kim@gmail.com>
2013-05-03 12:46:25 -0700

    Users can register and see their gift cards

--- app/controllers/gift_cards_controller.rb
+++ app/controllers/gift_cards_controller.rb
@@ -1,9 +1,23 @@
 class GiftCardsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @gift_cards = @user.gift_cards
+    @gift_cards = current_user.gift_cards
   end
 
-  def buy_gift_card
+  def new
+    @gift_card = current_user.gift_cards.build
   end
+
+  def create
+    @gift_card = GiftCard.new(params[:gift_card])
+    @gift_card.user = current_user
+    
+    if @gift_card.save
+      flash[:notice] = "Thanks for registering your gift card!"
+      redirect_to user_gift_cards_path(current_user)
+    else
+      flash[:notice] = "Sorry. Your gift card did not register"
+      redirect_to user_gift_cards_path(current_user)
+    end
+  end
+
 end
