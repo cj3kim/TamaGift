1ca956a5ea2e8708cf52ce1d5c1115de887bd582
Chris Kim <cj3kim@gmail.com>
2013-05-02 22:15:07 -0700

    Specced out gift card model with only it and describe blocks

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -3,4 +3,16 @@ class GiftCard < ActiveRecord::Base
 
   attr_accessible :vendor, :card_value, :card_code,
     :recipient_email, :recipient_first_name, :recipient_last_name, :user
+
+  def get
+  end
+
+  def create
+  end
+
+  def card_value_in_dollars
+  end
+
+  def generate_card_code
+  end
 end
--- spec/models/gift_card_spec.rb
+++ spec/models/gift_card_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe GiftCard do
+
+  before(:each) do 
+  end
+
+  describe "#get" do
+    it "retrieve a gift card object"
+  end
+
+  describe "#create" do 
+    it "generates a card code"
+  end
+
+  describe "#card_value_in_dollars" do
+    it "represents the card value in dollars rather than pennies"
+  end
+
+  describe "generate_card_code" do
+    it "generates a card code by hashing the vendor name and the last gift card sequence number"
+  end
+
 end
