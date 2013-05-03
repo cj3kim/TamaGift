32203664f4c1da0a843e2e430b5d6ebf507f635c
Chris Kim <cj3kim@gmail.com>
2013-05-03 00:22:53 -0700

    Tested the after_create callback in the Gift Model

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -1,14 +1,9 @@
 class GiftCard < ActiveRecord::Base
   belongs_to :user
+  after_create :generate_card_code
 
   attr_accessible :vendor, :card_value, :card_code,
     :recipient_email, :recipient_first_name, :recipient_last_name, :user
-
-  def get
-  end
-
-  def create
-  end
 
   def pennies_to_dollars(pennies)
     float_pennies = pennies.to_f #pennies is an integer value
@@ -16,9 +11,9 @@ class GiftCard < ActiveRecord::Base
     float_pennies / divisor
   end
 
-  def generate_card_code(vendor_name)
-    vendor_plus_time = "#{vendor_name} #{Time.now}"
-    digest = Digest::MD5.hexdigest(vendor_plus_time)
+  def generate_card_code
+    vendor_name_plus_time = "#{self.vendor} #{Time.now}"
+    digest = Digest::MD5.hexdigest(vendor_name_plus_time)
     update_attribute(:card_code, digest)
   end
 end
--- spec/models/gift_card_spec.rb
+++ spec/models/gift_card_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe GiftCard do
-  let(:user_one) do 
-    FactoryGirl.create(:user, 
-                       email: "starbucks_fiend@gmail.com",
-                       first_name: "Keitaro",
-                       last_name:"Urashima") 
-  end
-
   let(:starbucks_gift_card) do 
     FactoryGirl.create(:gift_card,
                        vendor: "Starbucks",
@@ -20,12 +13,18 @@ describe GiftCard do
 
   let(:turtle_gift_card) { FactoryGirl.create(:gift_card, vendor: "Tamaland", card_value: 2000) }
 
-  describe "#get" do
-    it "retrieve a gift card object"
-  end
+  describe "#after_create" do 
+    context "when a gift card is created" do
+      it "generates a card code" do
+        gift_card = GiftCard.new(vendor: "Sushi")
 
-  describe "#create" do 
-    it "generates a card code"
+        gift_card.card_code.should == nil
+
+        gift_card.save
+
+        gift_card.card_code.class.should == String
+      end
+    end
   end
 
   describe "#pennies_to_dollars" do
@@ -46,22 +45,28 @@ describe GiftCard do
 
   describe "#generate_card_code" do
     it "generates a card code and updates the card_code attribute" do
+      current_time = Time.now
       vendor_name = turtle_gift_card.vendor
-      Time.stub(:now).and_return("12345")
+
+      #Stubbing Time.now makes update_attribute to error 
+
+      number_string = "12345"
+      number_string.should_receive(:utc).and_return(current_time)
+      Time.stub(:now).and_return(number)
       card_code = "8804907b912e0de01d8f336f39ec02ee"
 
-      turtle_gift_card.card_code.should == nil
-      turtle_gift_card.generate_card_code(vendor_name)
-      turtle_gift_card.card_code.should_not == nil
+      turtle_gift_card.generate_card_code
+
       turtle_gift_card.card_code.should == card_code
 
-      Time.stub(:now).and_return("54321")
+      number_string = "54321"
+      number_string.should_receive(:utc).and_return(current_time)
+      Time.stub(:now).and_return(number)
       card_code = "a9566f704ad504c8c98dae9c5bf7e212"
 
-      turtle_gift_card.generate_card_code(vendor_name)
-      turtle_gift_card.card_code.should_not == nil
+      turtle_gift_card.generate_card_code
+
       turtle_gift_card.card_code.should == card_code
     end
   end
-
 end
